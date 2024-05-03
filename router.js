3c01b52828c1724cd5a776b621f4cd5f85ae805f
jominr <jominr@qq.com>
2024-05-03 18:14:38 +1000

    operate data

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const userList = [{
+const stuList = [{
   id: '1',
   attributes: {
     name: '悟空',
@@ -36,26 +36,98 @@ const userList = [{
   }
 }];
 
+const getStuById = (id) => {
+  return stuList.find(item => item.id == id);
+}
+
+const addStudent = (stuData) => {
+  const id = stuList.length + 1;
+  const data = {id, attributes: {...stuData}};
+  stuList.push(data);
+  return data;
+}
+
+const updata = (id, stuData) => {
+  const data = stuList.find(item => item.id === id);
+  data.attributes = {...stuData};
+  return data;
+}
+
+const delStu = (id) => {
+  const data = stuList.find(item => item.id === id);
+  const index = stuList.findIndex(item => item.id === id);
+  stuList.splice(index, 1);
+  return data
+}
+
+const user = {
+  username: 'min qiao',
+  psd: '123',
+  email: 'mm@qq.com'
+}
+
+
 router.get('/error', (req, res)=> {
   throw new Error('自定义的错误')
 })
 
-router.get('/user/list', (req, res)=> {
-  const query = req.query
+router.post('/auth/register', (req, res)=> {
   res.send({
     status: 0,
-    msg: 'get user list success',
-    data: userList
+    msg: 'register success',
+    data: req.body
   })
 })
 
-router.post('/user/list', (req, res)=> {
-  // 请求体数据
-  console.log(req.body);
+router.post('/auth/login', (req, res)=> {
   res.send({
     status: 0,
-    msg: 'post user list success',
-    data: req.body
+    msg: 'register success',
+    data: user,
+  })
+})
+
+
+router.get('/students', (req, res)=> {
+  res.send({
+    status: 0,
+    msg: 'get student list success',
+    data: stuList
+  })
+})
+
+router.get('/students/:id', (req, res)=> {
+  const data = getStuById(req.params.id);
+  res.send({
+    status: 0,
+    msg: 'get student by id success',
+    data: data
+  })
+})
+
+router.post('/students/', (req, res)=> {
+  // 请求体数据
+  const data = addStudent(req.body);
+  res.send({
+    status: 0,
+    msg: 'add student success',
+    data: data
+  })
+})
+
+router.put('/students/:id', (req, res)=> {
+  res.send({
+    status: 0,
+    msg: 'update student success',
+    data: updata(req.params.id, req.body)
+  })
+})
+
+router.delete('/students/:id', (req, res)=> {
+  res.send({
+    status: 0,
+    msg: 'delete student success',
+    data: delStu(req.params.id)
   })
 })
 
